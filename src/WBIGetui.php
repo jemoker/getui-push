8d141758052a372114aa49c4f2d5f4eddcb21600
jemoker <867119121@qq.com>
2016-03-10 17:38:22 +0800

    Update WBIGetui.php

--- src/WBIGetui.php
+++ src/WBIGetui.php
@@ -8,7 +8,7 @@ use IGtSingleMessage;
 use IGtTarget;
 use IGtAppMessage;
 use IGtTransmissionTemplate;
-require __DIR__.'/../vendor/autoload.php';
+
 require __DIR__.'/IGeTui.php';
 
 class WBIGetui
